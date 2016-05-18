b22a26d4a52388654c9f2bb731360624027d3e4d
andrepolischuk <andre.polischuk@gmail.com>
2016-05-18 14:28:01 +0300

    Initial commit

---
+++ README.md
@@ -0,0 +1,10 @@
+# neat
+
+> Minimal zsh prompt inspired by [odin][odin] and [pure][pure]
+
+## License
+
+MIT
+
+[odin]: https://github.com/tylerreckart/Odin
+[pure]: https://github.com/sindresorhus/pure
---
+++ neat.zsh
@@ -0,0 +1,29 @@
+PROMPT='$(prompt_user_host)%F{blue}%~%f $(git_prompt_status)$(git_prompt_info)%(?.%F{magenta}.%F{red})❯%f '
+
+prompt_user_host() {
+  if [[ -n $SSH_CONNECTION ]]; then
+    me="%n@%m"
+  elif [[ $LOGNAME != $USER ]]; then
+    me="%n"
+  fi
+  if [[ -n $me ]]; then
+    echo "%F{magenta}$me%f:"
+  fi
+}
+
+ZSH_THEME_GIT_PROMPT_PREFIX="%B%F{grey}"
+ZSH_THEME_GIT_PROMPT_SUFFIX="%f%b"
+ZSH_THEME_GIT_PROMPT_DIRTY=" %F{red}✗ %f"
+ZSH_THEME_GIT_PROMPT_UNTRACKED="%F{grey}◒ %f"
+ZSH_THEME_GIT_PROMPT_CLEAN=" "
+ZSH_THEME_GIT_PROMPT_ADDED="%F{cyan}✓ %f"
+ZSH_THEME_GIT_PROMPT_MODIFIED="%F{yellow}⚑ %f"
+ZSH_THEME_GIT_PROMPT_DELETED="%F{red}✖ %f"
+ZSH_THEME_GIT_PROMPT_RENAMED="%F{blue}➜ %f"
+ZSH_THEME_GIT_PROMPT_UNMERGED="%F{cyan}§ %f"
+ZSH_THEME_GIT_PROMPT_AHEAD="%F{blue}𝝙 %f"
+
+ZSH_THEME_GIT_TIME_SINCE_COMMIT_SHORT="%F{green}"
+ZSH_THEME_GIT_TIME_SHORT_COMMIT_MEDIUM="%F{yellow}"
+ZSH_THEME_GIT_TIME_SINCE_COMMIT_LONG="%F{red}"
+ZSH_THEME_GIT_TIME_SINCE_COMMIT_NEUTRAL="%F{gray}"
