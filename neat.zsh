40237be8916465ab6f109d3f7e491e70dd1c05fc
Andrey Polischuk <andre.polischuk@gmail.com>
2016-10-05 14:43:23 +0300

    Add username for root and tweaks

--- neat.zsh
+++ neat.zsh
@@ -1,18 +1,53 @@
-PROMPT='$(prompt_user_host)%F{cyan}%~%f $(git_prompt_status)$(git_prompt_info)%(?.%F{yellow}.%F{red})❯%f '
-RPROMPT='$(prompt_battery_status)'
+# Git status symbols
+ZSH_THEME_GIT_PROMPT_PREFIX="%B%F{grey}"
+ZSH_THEME_GIT_PROMPT_SUFFIX="%f%b "
+ZSH_THEME_GIT_PROMPT_UNTRACKED="%F{grey}⋯ %f"
+ZSH_THEME_GIT_PROMPT_ADDED="%F{green}✓ %f"
+ZSH_THEME_GIT_PROMPT_MODIFIED="%F{yellow}⚑ %f"
+ZSH_THEME_GIT_PROMPT_DELETED="%F{red}✖ %f"
+ZSH_THEME_GIT_PROMPT_RENAMED="%F{blue}➜ %f"
+ZSH_THEME_GIT_PROMPT_UNMERGED="%F{red}‼ %f"
+ZSH_THEME_GIT_PROMPT_AHEAD="%F{blue}↑ %f"
+ZSH_THEME_GIT_PROMPT_BEHIND="%F{blue}↓ %f"
 
-function prompt_user_host() {
-  if [[ -n $SSH_CONNECTION ]]; then
-    me="%n@%m"
-  elif [[ $LOGNAME != $USER ]]; then
-    me="%n"
+# Username
+neat_user() {
+  if [[ $USER == 'root' ]]; then
+    echo -n "%B%F{red}"
+  else
+    echo -n "%B%F{magenta}"
   fi
-  if [[ -n $me ]]; then
-    echo "%F{magenta}$me%f:"
+
+  echo -n "%n%f%b"
+}
+
+# Username and ssh host
+neat_host() {
+  if [[ -n $SSH_CONNECTION ]]; then
+    echo -n "$(neat_user)"
+    echo -n "%B%F{grey}@%m%f%b "
+  elif [[ $LOGNAME != $USER ]] || [[ $USER == 'root' ]]; then
+    echo -n "$(neat_user) "
   fi
 }
 
-function prompt_battery_status() {
+# Current directory
+neat_current_dir() {
+  echo -n "%F{cyan}%~%f "
+}
+
+# Git status
+neat_git_status() {
+  echo -n "$(git_prompt_status)$(git_prompt_info)"
+}
+
+# Prompt symbol
+neat_return_status() {
+  echo -n "%(?.%F{yellow}.%F{red})❯%f "
+}
+
+# Battery status
+neat_battery_status() {
   if [[ `uname` == 'Linux' ]]; then
     current_charge=$(cat /proc/acpi/battery/BAT1/state | grep 'remaining capacity' | awk '{print $3}')
     total_charge=$(cat /proc/acpi/battery/BAT1/info | grep 'last full capacity' | awk '{print $4}')
@@ -29,20 +64,16 @@ function prompt_battery_status() {
   if [[ $charged_percentage -gt 100 ]]; then
     charged_percentage=100
   fi
+
   if [[ $charged_percentage -lt 25 ]]; then
     if [ -z $is_charging ]; then
-      echo "%F{red}⚡%f"
+      echo -n "%F{red}⚡%f"
     fi
   fi
 }
 
-ZSH_THEME_GIT_PROMPT_PREFIX="%B%F{grey}"
-ZSH_THEME_GIT_PROMPT_SUFFIX="%f%b "
-ZSH_THEME_GIT_PROMPT_UNTRACKED="%F{grey}⋯ %f"
-ZSH_THEME_GIT_PROMPT_ADDED="%F{green}✓ %f"
-ZSH_THEME_GIT_PROMPT_MODIFIED="%F{yellow}⚑ %f"
-ZSH_THEME_GIT_PROMPT_DELETED="%F{red}✖ %f"
-ZSH_THEME_GIT_PROMPT_RENAMED="%F{blue}➜ %f"
-ZSH_THEME_GIT_PROMPT_UNMERGED="%F{red}‼ %f"
-ZSH_THEME_GIT_PROMPT_AHEAD="%F{blue}↑ %f"
-ZSH_THEME_GIT_PROMPT_BEHIND="%F{blue}↓ %f"
+# Compose prompt
+PROMPT='$(neat_host)$(neat_current_dir)$(neat_git_status)$(neat_return_status)'
+
+# Compose right prompt
+RPROMPT='$(neat_battery_status)'
